7d47d2f0e7b61c23d85c6f9356a6e3a041e8b6b4
Toomas Korka <evyumble@gmail.com>
2013-05-28 02:58:49 +0300

    Update Mang.java

--- src/Mang.java
+++ src/Mang.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class Mang {
 	public static char [] TahedInimene = new char [7];
-	public static char [] TahedAI =  new char [7];
+	public static char [] TahedAI = new char [7];
 	public static String[][] MangulaudMassiiv = Mangulaud.LooMangulaud();
 	public static int AISkoor = 0;
 	public static int InimeseSkoor = 0;
@@ -21,7 +21,7 @@ public class Mang {
 	public static ArrayList<String> sonaraamat = new ArrayList<String>();
 	public static ArrayList<char[]> charid = new ArrayList<char[]>();
 	static GUI myGame;
-	
+
 	public static void main(String[] args) throws IOException{
 		failistJarjendisse();
 		Tahed2.setKott();
@@ -33,32 +33,32 @@ public class Mang {
 			TahedAI[i] = Tahed2.kott(TaheKott);
 		}
 		myGame = new GUI();
-			//1. m2ngija
-			//kasutatudTahed.clear();
-			myGame.setTähed(TahedInimene);
+		//1. m2ngija
+		//kasutatudTahed.clear();
+		myGame.setTähed(TahedInimene);
 	}
-	
+
 	public static char[] removeElements(char[] input, char deleteMe) {
 		if (input.length==1) {
 			return new char[0];
 		}
-	    char [] result = new char[input.length-1];
-	    int i = 0;
-	    for(char item : input){
-	    	if(deleteMe!=item && item!='\0'){
-	            result[i]=item;
-	            System.out.println("alles veel täht " + item);
-	            i+=1;
-	        }
-	        else break;
-	    }
-	    System.out.println("tsükkel läbi ");
-	    for (;i<result.length; i++){
-	    	 result[i]=input[i+1];
-	    }
-	    return result;
+		char [] result = new char[input.length-1];
+		int i = 0;
+		for(char item : input){
+			if(deleteMe!=item && item!='\0'){
+				result[i]=item;
+				System.out.println("alles veel täht " + item);
+				i+=1;
+			}
+			else break;
+		}
+		System.out.println("tsükkel läbi ");
+		for (;i<result.length; i++){
+			result[i]=input[i+1];
+		}
+		return result;
 	}
-	
+
 	static boolean mangKestab() {
 		// TODO Auto-generated method stub
 		if (TahedInimene.length == 0 || TahedAI.length == 0) return false;
@@ -69,7 +69,7 @@ public class Mang {
 		System.out.println("Tahtede haldamine input " + input);
 		inputObjekt = new InfoInputist(input);
 		System.out.println("sõne ise " + inputObjekt.sõne_ise);
-		//nyyd tuleb reaalne tabelisse kirjutamine 
+		//nyyd tuleb reaalne tabelisse kirjutamine
 		punktid = 0;
 		sonaKordaja = 1;
 		kasutatudTahed.clear();
@@ -114,7 +114,7 @@ public class Mang {
 					//myGame.textArea1.append("tähe väärtus on "+ Tahed.vaartus(inputObjekt.sõne_ise.charAt(indeks)));
 
 				}
-				else{ 
+				else{
 					punktid = punktid+ Tahed.vaartus(inputObjekt.sõne_ise.charAt(indeks));
 					kasutatudTahed.add(inputObjekt.sõne_ise.charAt(indeks));
 					//myGame.textArea1.append("lisati " + inputObjekt.sõne_ise.charAt(indeks)+ " punktid" + punktid);
@@ -169,26 +169,26 @@ public class Mang {
 		punktid *= sonaKordaja;
 		//myGame.textArea1.append("punkte " + punktid);
 	}
-	
+
 	/*public static Boolean tahedKlapivad(String elem, String sona, String tahed){
-		kasutatudTahed.clear();
-		int indeks = sona.indexOf(elem);
-    	for (int k = 0; k < sona.length(); k++){
-    		if (k < indeks || k > indeks-1+elem.length()){
-    			kasutatudTahed.add(sona.toCharArray()[k]);
-    		}
-		}
-    	for (char taht : kasutatudTahed){
-    		if (tahed.indexOf(taht)<0){
-    			kasutatudTahed.clear();
-    			return false;
-    		}
-    		else tahed = tahed.replace(taht, '0');
-    	}
-    	kasutatudTahed.clear();
-    	return true;
-	}
-	*/
+kasutatudTahed.clear();
+int indeks = sona.indexOf(elem);
+for (int k = 0; k < sona.length(); k++){
+if (k < indeks || k > indeks-1+elem.length()){
+kasutatudTahed.add(sona.toCharArray()[k]);
+}
+}
+for (char taht : kasutatudTahed){
+if (tahed.indexOf(taht)<0){
+kasutatudTahed.clear();
+return false;
+}
+else tahed = tahed.replace(taht, '0');
+}
+kasutatudTahed.clear();
+return true;
+}
+	 */
 	public static void failistJarjendisse() throws IOException{
 		String pwd = System.getProperty("user.dir");
 		File sonadFile = new File(pwd + "/src/sonad.txt");
