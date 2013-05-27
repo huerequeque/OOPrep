f576b43a00665f04d9e3836dda8350fab38a9f12
Toomas Korka <evyumble@gmail.com>
2013-05-28 02:57:26 +0300

    Update AI.java

--- src/AI.java
+++ src/AI.java
@@ -3,16 +3,13 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
-
-
 public class AI implements Runnable{
 	public static ArrayList<String> potentsiaalsed = new ArrayList<String>();
 
 	public String SkaneeriLauda(String inimeseSisend) throws FileNotFoundException{
 		if (!inimeseSisend.equals("jääb vahele")){
 			String[] jupid = inimeseSisend.split(" ");
-			int rida = Integer.parseInt(jupid[0]);  //index
+			int rida = Integer.parseInt(jupid[0]); //index
 			int tulp = Integer.parseInt(jupid[1]); //index
 			String p_v_a = jupid[2];
 			String sõne_ise = jupid[3];
@@ -21,7 +18,7 @@ public class AI implements Runnable{
 				//skaneerin ridu, et pyyda t2hti ja s6naosasid, mida kasutades v6iks AI k2igu sooritafa
 				String pyydja = "";
 				for (int j = tulp-1; j < tulp+1; j++){
-					while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){					
+					while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){	
 						pyydja+=Mang.MangulaudMassiiv[i][j];
 						if (j < Mang.MangulaudMassiiv.length) j+=1;
 
@@ -47,7 +44,7 @@ public class AI implements Runnable{
 				//skaneerin tulpi samal eesm2rgil
 				String pyydja = "";
 				for (int i = rida-1; i < rida+1; i++){
-					while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){					
+					while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){	
 						pyydja+=Mang.MangulaudMassiiv[i][j];
 						if (i < Mang.MangulaudMassiiv.length) i+=1;
 					}
@@ -105,7 +102,7 @@ public class AI implements Runnable{
 		for (loendur=0; loendur < Mang.charid.size(); loendur++){
 			char [] sonaCharideks = Mang.charid.get(loendur);
 			if (sonaCharideks.length<=8){
-				boolean sisaldabKoiki = false; 
+				boolean sisaldabKoiki = false;
 				for (char charElem: sonaCharideks){
 					//System.out.println(""+charElem);
 					for (char charElem2: tahedJaElement){
@@ -124,13 +121,13 @@ public class AI implements Runnable{
 			}
 		}
 
-		//	for (loendur=0; loendur < Mang.sonaraamat.size(); loendur++){
+		// for (loendur=0; loendur < Mang.sonaraamat.size(); loendur++){
 		for (int indeks : indeksid){
 			String sona = Mang.sonaraamat.get(indeks);
-			Pattern p = Pattern.compile(StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7) + elem + StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7));
-			Matcher m = p.matcher(sona);
+			//Pattern p = Pattern.compile(StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7) + elem + StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7));
+			//Matcher m = p.matcher(sona);
 			//kasutan kontrollimeetodeid sobivate peal
-			if (m.matches() && sona.length() > 3 && (sona.length() > elem.length() || Mang.voor==0)) {
+			if (sona.length() > 3 && (sona.length() > elem.length() || Mang.voor==0)) {
 				//System.outprintln("tahed klapivad");
 				if (j-1-sona.indexOf(elem)>= 0 && p_v_a=="p" && AI_kontroll.kontroll(i+ ", " + (j-1-sona.indexOf(elem)) + " " + p_v_a + " " + sona + " AI")){
 					if (!potentsiaalsed.contains((i+ ", " + (j-1-sona.indexOf(elem)) + " " + p_v_a + " " + sona + " AI")) && !matches.contains((i+ ", " + (j-1-sona.indexOf(elem)) + " " + p_v_a + " " + sona + " AI"))){
@@ -145,18 +142,18 @@ public class AI implements Runnable{
 				}
 
 				//kui k6ik sobib, esitan vastuse n6utud inputi formaadis
-			}			    
+			}	
 		}
 
 		//kui 3st tähest pikemaid sõnu ei leidnud, otsin kesisemat varianti
 		if (matches.size()==0){
 			for (int indeks : indeksid){
 				String sona = Mang.sonaraamat.get(indeks);
-				//otsin s6nastikust regex mustri ja m2ngija t2htedega klappivaid s6nu 
-				Pattern p = Pattern.compile(StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7) + elem + StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7));
-				Matcher m = p.matcher(sona);
+				//otsin s6nastikust regex mustri ja m2ngija t2htedega klappivaid s6nu
+				//Pattern p = Pattern.compile(StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7) + elem + StringUtils.repeat(("[" + TahedString + "]{0,1}"), 7));
+				//Matcher m = p.matcher(sona);
 				//kasutan kontrollimeetodeid sobivate peal
-				if (m.matches() && (sona.length() > elem.length() || Mang.voor==0)) {
+				if (sona.length() > elem.length() || Mang.voor==0) {
 					//System.outprintln("tahed klapivadsa");
 					//System.outprintln("kontrollin seda " + i+ ", " + (j-1-sona.indexOf(elem)) + " " + p_v_a + " " + sona);
 					if (j-1-sona.indexOf(elem)>= 0 && p_v_a=="p" && AI_kontroll.kontroll(i+ ", " + (j-1-sona.indexOf(elem)) + " " + p_v_a + " " + sona + " AI")){
@@ -171,13 +168,13 @@ public class AI implements Runnable{
 							//System.outprintln("sobis " + (i-1-sona.indexOf(elem))+ ", " + j + " " + p_v_a + " " + sona);
 						}
 					}
-				}  	
+				}
 				//kui k6ik sobib, esitan vastuse n6utud inputi formaadis
-			}			    
+			}	
 
 		}
 		//teen klappivatest suvalise valiku
-		if (matches.size() == 0) return "0, 0 p xxxx";		
+		if (matches.size() == 0) return "0, 0 p xxxx";	
 		else {
 			String pakkumine = matches.get((int)(Math.random()*matches.size()));
 			matches.remove(pakkumine);
@@ -189,19 +186,19 @@ public class AI implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		/*
-		for (int i = 0; i < potentsiaalsed.size(); i ++){
-			if (!AI_kontroll.kontroll(potentsiaalsed.get(i) + " AI")){
-				potentsiaalsed.remove(potentsiaalsed.get(i));
-				i-=1;
-			}
-		}
+for (int i = 0; i < potentsiaalsed.size(); i ++){
+if (!AI_kontroll.kontroll(potentsiaalsed.get(i) + " AI")){
+potentsiaalsed.remove(potentsiaalsed.get(i));
+i-=1;
+}
+}
 		 */
 		for (int i = 0; i < Mang.MangulaudMassiiv.length; i++){
 			//skaneerin ridu, et pyyda t2hti ja s6naosasid, mida kasutades v6iks AI k2igu sooritafa
 			String pyydja = "";
 			if (Mang.TahedAI.length==0) break;
 			for (int j = 0; j < Mang.MangulaudMassiiv.length; j++){
-				while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){					
+				while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){	
 					pyydja+=Mang.MangulaudMassiiv[i][j];
 					if (j < Mang.MangulaudMassiiv.length) j+=1;
 				}
@@ -225,7 +222,7 @@ public class AI implements Runnable{
 			if (Mang.TahedAI.length==0) break;
 			String pyydja = "";
 			for (int i = 0; i < Mang.MangulaudMassiiv.length; i++){
-				while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){					
+				while (i<15 && j<15 && !Mang.MangulaudMassiiv[i][j].equals(" ") && !Mang.MangulaudMassiiv[i][j].equals("2xs") && !Mang.MangulaudMassiiv[i][j].equals("3xs") && !Mang.MangulaudMassiiv[i][j].equals("3xt") && !Mang.MangulaudMassiiv[i][j].equals("2xt")){	
 					pyydja+=Mang.MangulaudMassiiv[i][j];
 					if (i < Mang.MangulaudMassiiv.length) i+=1;
 				}
